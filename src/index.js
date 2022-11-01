4a7a4cc960eb1ee4545ab0de047340e87d4b47f0
sawfiz <dashu.up@gmail.com>
2022-11-01 11:39:04 +0800

    Add add task button modal and eventlisener

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-console.log('This works!');
+const toToApp = (() => {
+  console.log('This works!');
+  const bigAddBtn = document.querySelector('.big-add');
+  const overlayEl = document.querySelector('.overlay');
 
+  // Modal functions
+  function openModal(modal) {
+    if (modal === null) return;
+    modal.classList.add('active');
+    overlayEl.classList.add('active');
+  }
+
+  function closeModal(modal) {
+    if (modal === null) return;
+    modal.classList.remove('active');
+    overlayEl.classList.remove('active');
+  }
+
+  bigAddBtn.addEventListener('click', () => {
+    // Modals and overlay
+    const addTaskDialog = document.querySelector('#add-task-dialog');
+    console.log(addTaskDialog);
+    openModal(addTaskDialog);
+  });
+
+  // Click outside of a popup closes the popup
+  overlayEl.addEventListener('click', () => {
+    const modalsEl = document.querySelectorAll('.modal.active');
+    modalsEl.forEach((modal) => {
+      closeModal(modal);
+    });
+  });
+})();
